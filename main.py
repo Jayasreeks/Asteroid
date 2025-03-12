2618cb59e3ed63362fcafa028a8aee086fc9a2f4
Jayasreeks <jayasree.ks2k04@gmail.com>
2025-03-12 21:28:04 +0530

    added rotation and movement event using key events

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import Player # player is a module and Player is the class inside it so importing only the class 
+
 
 def main():
     pygame.init()
@@ -10,15 +12,24 @@ def main():
     clock = pygame.time.Clock()
     dt=0
     
+    # drawing the player after the screen appears and b4 flipping the screen
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) # creating player pbject before loop 
+    
     running= True
     while running:
         for event in pygame.event.get(): # will get all the events from the screen 
             if event.type == pygame.QUIT:
                 running = False
+        #update player state
+        player1.update(dt)
+        
+        #render everything from here        
         screen.fill(color="black")
+        player1.draw(screen)
         pygame.display.flip() 
         
         dt=clock.tick(60)/1000 #Calculate delta time at the end
+        
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
