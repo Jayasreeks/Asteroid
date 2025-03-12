1e1715ad8157b50d8037d5dfe9ce07de197eff14
Jayasreeks <jayasree.ks2k04@gmail.com>
2025-03-12 10:17:13 +0530

    Updated file structure

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+
+
+if __name__ == "__main__":
+    main()
+    """
+This line ensures the main() function is only called when this
+file is run directly; it won't run if it's imported as a module.
+It's considered the "pythonic" way to structure an executable program
+in Python.
+
+
+    """
+    
+
